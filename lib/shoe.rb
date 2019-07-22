462bdb321dcb2b420059217c0a4d68b99e1358f9
Chris Cheong <chrisxcheong@gmail.com>
2019-07-22 21:24:19 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,16 @@
 # Make your shoe class here!
+class Shoe
+attr_accessor :brand, :color, :size, :material, :condition
+
+def initialize(brand)
+  @brand = brand
+end
+
+def cobble
+  puts "She has been repaired."
+  @condition = "New"
+end
+  
+end
+
+end
